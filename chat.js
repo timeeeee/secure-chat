e9dbcae898ff7625136430c8124aaf0c0eda6f88
Tim Clark <tim.clark@propylon.com>
2015-01-12 17:05:19 -0600

    change to username/password, test encryption

--- chat.js
+++ chat.js
@@ -2,11 +2,18 @@ function getkey() {
     // get first and only file from filelist object
     var reader = new FileReader();
 
+    var password = $("input#username").val();
+    var hash = CryptJS.SHA256(password);
+    var double_hash = CryptJS.SHA256(hash);
+    var encrypted = CryptJS.AES.encrypt("private key", hash);
+
+    alert("double hash: " + double_hash + "\nencrypted private key: " + encrypted);
+
     // Hide login, reveal chat window
     $("form#login").hide();
     $("div#chat").show();
 }
 
 $(document).ready(function() {
-    $("input#keyfile").change(getkey);
+    $("button#login").click(getkey);
 });
