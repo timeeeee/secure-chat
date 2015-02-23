94d01b93407e360ba0a930b927e9cbd4877953b0
Tim Clark <timeeeee@gmail.com>
2015-02-23 12:15:34 -0600

    replace server.py with wsgi server, fill in some server methods

--- server.py
+++ server.py
@@ -1,47 +1,60 @@
-import socket
+#! /usr/bin/env python
 
-response = "The server send this sentence as a response."
+from wsgiref.simple_server import make_server
+import sqlite3
+import json
 
-# Standard socket stuff:
-host = ''  # do we need socket.gethostname() ?
-port = 53421
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind((host, port))
-sock.listen(1)  # don't queue up any requests
- 
-# Loop forever, listening for requests:
-print "going to enter loop"
-while True:
-    csock, caddr = sock.accept()
-    print "Connection from: " + `caddr`
-    req = csock.recv(1024)  # get the request, 1KB max
-    print req
-    print "type: {}".format(type(req))
-    csock.sendall(response)
-    csock.close()
-'''
-    # Look in the first line of the request for a move command
-    # A move command should be e.g. 'http://server/move?a=90'
+database = sqlite3.connect("chat.db")
 
-    match = re.match('GET /move\?a=(\d+)\sHTTP/1', req)
-    if match:
-        angle = match.group(1)
-        print "ANGLE: " + angle + "\n"
-        csock.sendall("""HTTP/1.0 200 OK
-Content-Type: text/html
- 
-<html>
-<head>
-<title>Success</title>
-</head>
-<body>
-Boo!
-</body>
-</html>
-""")
-    else:
-        # If there was no recognised command then return a 404 (page not found)
-        print "Returning 404"
-        csock.sendall("HTTP/1.0 404 Not Found\r\n")
-    csock.close()
-'''
+# This is our application object. It could have any name,
+# except when using mod_wsgi where it must be "application"
+def application(environ, start_response):
+    if environ["PATH_INFO"] == "/":
+        # build the response body possibly using the environ dictionary
+        response_body = '<br>'.join("%s: %s" % (key, value)
+                                 for key, value in sorted(environ.items()))
+        status = "200 OK"
+        response_headers = [('Content-Type', 'text/html'),
+                            ('Content-Length', str(len(response_body)))]
+
+    # Get userlist
+    elif environ["PATH_INFO"] == "/userlist":
+        response_body = json.dumps(["Bryan", "Jimbo"])
+        status = "200 OK"
+        response_headers = [('Content-Type', 'application/json'),
+                            ('Content-Length', str(len(response_body)))]
+
+    # Update - get new messages and online users
+    elif environ["PATH_INFO"] == "/update":
+        response_body = json.dumps({"online": ["Jimbo"], "new_messages": ["this is a TOTALLY ENCRYPTED message"]})
+        status = "200 OK"
+        response_headers = [('Content-Type', 'application/json'),
+                            ('Content-Length', str(len(response_body)))]
+
+    elif environ["PATH_INFO"] == "/oldmessages":
+        old_messages = ["encrypted message 1", "encrypted message 2", "encrypted message 3"]
+        response_body = json.dumps(old_messages)
+        status = "200 OK"
+        response_headers = [('Content-Type', 'application/json'),
+                            ('Content-Length', str(len(response_body)))]
+
+    elif environ["PATH_INFO"] == "/sendmessage":
+        try:
+            request_body_size = int(environ["CONTENT_LENGTH"])
+        except(ValueError):
+            request_body_size = 0
+        message = environ["wsgi.input"].read(request_body_size)
+        response_body = json.dumps({"success": True})
+        status = "200 OK"
+        response_headers = [('Content-Type', 'application/json'),
+                            ('Content-Length', str(len(response_body)))]
+        
+
+    start_response(status, response_headers)
+    return [response_body]
+
+httpd = make_server('0.0.0.0', 53421, application)
+
+print "starting server"
+# httpd.serve_forever() # REALLY forever?!? could not kill with ctrl c
+httpd.handle_request()
--- wsgi_server.py
+++ wsgi_server.py
@@ -1,24 +1,56 @@
-#!! /usr/bin/env python
+#! /usr/bin/env python
 
 from wsgiref.simple_server import make_server
+import sqlite3
+import json
+
+database = sqlite3.connect("chat.db")
 
 # This is our application object. It could have any name,
 # except when using mod_wsgi where it must be "application"
 def application(environ, start_response):
-    # build the response body possibly using the environ dictionary
-    response_body = '<br>'.join("%s: %s" % (key, value)
-                             for key, value in sorted(environ.items()))
-#    response_body = "<html><body><h1>Hi!</h1><p>Some text</p></body></html>"
-    status = '200 OK'
- 
-    # [(Header name, Header value)].
-    response_headers = [('Content-Type', 'text/html'),
-                        ('Content-Length', str(len(response_body)))]
- 
+    if environ["PATH_INFO"] == "/":
+        # build the response body possibly using the environ dictionary
+        response_body = '<br>'.join("%s: %s" % (key, value)
+                                 for key, value in sorted(environ.items()))
+        status = "200 OK"
+        response_headers = [('Content-Type', 'text/html'),
+                            ('Content-Length', str(len(response_body)))]
+
+    # Get userlist
+    elif environ["PATH_INFO"] == "/userlist":
+        response_body = json.dumps(["Bryan", "Jimbo"])
+        status = "200 OK"
+        response_headers = [('Content-Type', 'application/json'),
+                            ('Content-Length', str(len(response_body)))]
+
+    # Update - get new messages and online users
+    elif environ["PATH_INFO"] == "/update":
+        response_body = json.dumps({"online": ["Jimbo"], "new_messages": ["this is a TOTALLY ENCRYPTED message"]})
+        status = "200 OK"
+        response_headers = [('Content-Type', 'application/json'),
+                            ('Content-Length', str(len(response_body)))]
+
+    elif environ["PATH_INFO"] == "/oldmessages":
+        old_messages = ["encrypted message 1", "encrypted message 2", "encrypted message 3"]
+        response_body = json.dumps(old_messages)
+        status = "200 OK"
+        response_headers = [('Content-Type', 'application/json'),
+                            ('Content-Length', str(len(response_body)))]
+
+    elif environ["PATH_INFO"] == "/sendmessage":
+        try:
+            request_body_size = int(environ["CONTENT_LENGTH"])
+        except(ValueError):
+            request_body_size = 0
+        message = environ["wsgi.input"].read(request_body_size)
+        response_body = json.dumps({"success": True})
+        status = "200 OK"
+        response_headers = [('Content-Type', 'application/json'),
+                            ('Content-Length', str(len(response_body)))]
+        
+
     start_response(status, response_headers)
- 
-    # Return the response body.
-    # Notice it is wrapped in a list although it could be any iterable.
     return [response_body]
 
 httpd = make_server('0.0.0.0', 53421, application)
