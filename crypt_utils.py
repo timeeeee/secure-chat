ddaf715514ae4cd714fc6f6ee4d68d8bfaa258de
Tim Clark <timeeeee@gmail.com>
2015-05-01 15:53:14 -0500

    added pad, unpad, aes_encrypt and aes_decrypt to crypt_utils, and fixed for pep8

--- crypt_utils.py
+++ crypt_utils.py
@@ -1,6 +1,7 @@
 from Crypto.PublicKey import RSA as CryptoRSA
 from Crypto.Hash import SHA256
 from Crypto.Cipher import AES
+from Crypto.Random.random import randint
 
 RSA_KEYSIZE = 2048
 PUBLIC_EXPONENT = 65537L
@@ -9,6 +10,24 @@ PUBLIC_EXPONENT = 65537L
 def sha(string):
     hash_object = SHA256.new(string)
     return hash_object.digest()
+
+
+def pad(string, block_size=16):
+    # The first 2 bytes of the string will indicate the length of the string
+    # Then append random bytes until the length is a multiple of the block size
+    length = len(string)
+    if length > 2**16 - 1:
+        raise ValueError("The maximum string size before padding is 65535.")
+    binary_length = chr(length / 256) + chr(length % 256)
+    pad_length = block_size - (length + 1) % block_size - 1
+    pad = "".join(chr(randint(0, 255)) for _ in range(pad_length))
+    return binary_length + string + pad
+
+
+def unpad(string):
+    tens, ones = string[:2]
+    length = ord(tens) * 256 + ord(ones)
+    return string[2:2 + length]
 
 
 class RSA:
@@ -31,14 +50,26 @@ class RSA:
 
     def decrypt(self, msg):
         if self.d is None:
-            raise NotImplementedError("This RSA object does not contain a private key.")
+            raise NotImplementedError(
+                "This RSA object does not contain a private key.")
         return self.key_object.decrypt(msg)
+
     def getPublicKey(self):
         return self.n
 
     def getPrivateKey(self):
         if self.d is None:
-            raise NotImplementedError("This RSA object does not contain a private key.")
+            raise NotImplementedError(
+                "This RSA object does not contain a private key.")
         return self.d
-    
-            
+
+
+def aes_encrypt(msg, key):
+    padded_msg = pad(msg)
+    aes_object = AES.new(sha(key))
+    return aes_object.encrypt(pad(msg))
+
+
+def aes_decrypt(msg, key):
+    aes_object = AES.new(sha(key))
+    return unpad(aes_object.decrypt(msg))
