a4afc50022a5c94f7623ec5978b310ccfe6c8952
timeeeee <timeeeee@gmail.com>
2017-02-10 23:16:12 -0600

    move sha -> sha512, add rsa_sign, rsa_check_signature

--- crypt_utils.py
+++ crypt_utils.py
@@ -4,9 +4,9 @@ from Crypto.Cipher import AES
 from Crypto.Random.random import randint
 
 
-def sha(string):
+def sha512(string):
     """
-    Calculate the 512 bit SHA hash of a string.
+    Calculate the 512 bit SHA-2 hash of a string.
     """
     hash_object = SHA512.new(string)
     return hash_object.digest()
@@ -22,6 +22,20 @@ def rsa_encrypt(msg, public_key):
 def rsa_decrypt(msg, private_key):
     """
     Decrypt a message with RSA using the recipient's private key.
+    """
+    pass
+
+
+def rsa_sign(string, private_key):
+    """
+    Sign a string with RSA using the sender's private key.
+    """
+    pass
+
+
+def rsa_check_signature(signature, public_key):
+    """
+    Verify an RSA signature with the sender's public key.
     """
     pass
 
